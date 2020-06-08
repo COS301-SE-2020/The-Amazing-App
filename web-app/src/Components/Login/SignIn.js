9ade764ef1687b947646f3a374eca5de7cf5dae1
Tshegofatso12-ai <62068842+Tshegofatso12-ai@users.noreply.github.com>
2020-06-09 00:29:19 +0200

    Updated render function

--- web-app/src/Components/Login/SignIn.js
+++ web-app/src/Components/Login/SignIn.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import loginImg from '../../location.PNG';
+import logo from '../../logo.png';
+import './../Login/login.css'
 
 class SignIn extends React.Component
 {
     render()
     {
-        return;
+        return (<div>
+            <img src={logo} alt="logo" id="logo"></img>
+            <div class = "loginPage" >
+                <span class="loginContainer">
+                    <div class = "logReg">
+                        <ul>
+                            <li><a class="active" href="login.html">Login</a></li>
+                            <li><a  href="register.html">Register</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                    Username: <input id="username" type="text" name="username"></input>
+                    Password: <input id="password" type="password" name="password"></input>
+                    <div id="input">
+                        <button id="loginbtn">Login</button>
+                        <a href="#">Forgot Password </a>
+                    </div>
+                </div>
+                </span> 
+            </div>
+            <img src={loginImg} alt="girl" id="image"></img>
+            </div>);
     }
 }
 
