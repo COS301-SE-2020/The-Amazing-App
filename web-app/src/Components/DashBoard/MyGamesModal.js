37dd099d1d4c93769c3954e796c45137c62a8dfc
tapehNdhlovu <47606210+tapehNdhlovu@users.noreply.github.com>
2020-06-09 00:25:22 +0200

    updated dashboard files

--- web-app/src/Components/DashBoard/MyGamesModal.js
+++ web-app/src/Components/DashBoard/MyGamesModal.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Button, Header, Modal } from "semantic-ui-react";
 
 class MyGamesModal extends React.Component {
   render() {
     return (
-      <button className="ui large button" style={{marginTop: 12,fontSize: 15,backgroundColor: "#2A9D8F",color: "white"}}>
-        <i className="trophy icon"></i>
-        My Games
-      </button>
+      <Modal
+        style={{top: "auto",bottom: "auto",left: "auto",right: "auto",position: "relative",height: 300}} 
+        trigger={
+          <Button style={{marginTop: 12,fontSize: 15,backgroundColor: "#2A9D8F",color: "white", width: 155}}>
+            <i className="trophy icon"></i>
+            My Games
+          </Button>
+        } centered={false} closeIcon>
+        <Modal.Header>My Games</Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+            <Header></Header>
+            <p></p>
+            <p></p>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     );
   }
 }
--- web-app/src/Components/DashBoard/NotificationsModal.js
+++ web-app/src/Components/DashBoard/NotificationsModal.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Button, Header, Modal } from "semantic-ui-react";
 
 class NotificationsModal extends React.Component {
   render() {
     return (
-      <button className="ui large button"style={{marginTop: 12,fontSize: 15,backgroundColor: "#2A9D8F",color: "white"}}>
-        <i className="bell icon"></i>
-        Notifications
-      </button>
+      <Modal
+        style={{top: "auto",bottom: "auto",left: "auto",right: "auto",position: "relative",height: 300}} 
+        trigger={
+          <Button style={{marginTop: 12,fontSize: 15,backgroundColor: "#2A9D8F",color: "white",}}>
+            <i className="bell icon"></i>
+            Notifications
+          </Button>
+        } centered={false} closeIcon>
+        <Modal.Header>Notifications</Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+            <Header></Header>
+            <p></p>
+            <p></p>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     );
   }
 }
