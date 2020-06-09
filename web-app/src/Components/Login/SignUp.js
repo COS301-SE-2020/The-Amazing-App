c47886c24ef1023e0112bfcffd504632e6ec100c
Tshegofatso <u18189424@tuks,co.za>
2020-06-10 00:53:10 +0200

    server created

--- web-app/src/Components/Login/SignUp.js
+++ web-app/src/Components/Login/SignUp.js
@@ -2,9 +2,56 @@ import React from 'react';
 import loginImg from '../../location.PNG';
 import logo from '../../logo.png';
 import './login.css'
+import axios from 'axios';
 
-class SignUp extends React.Component
-{
+export default class SignUp extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onChangeUserEmail = this.onChangeUserEmail.bind(this);
+        this.onChangeUserPassword = this.onChangeUserPassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    onChangeUserName(e) {
+        this.setState({ name: e.target.value })
+    }
+
+    onChangeUserEmail(e) {
+        this.setState({ email: e.target.value })
+    }
+
+    onChangeUserPassword(e) {
+        this.setState({ password: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+
+        const userObject = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        axios.post('http://localhost:4000/users/create', userObject)
+            .then((res) => {
+                console.log(res.data)
+            }).catch((error) => {
+                console.log(error)
+            });
+
+        this.setState({ name: '', email: '', password: ''})
+    }
+
     render()
     {
         return(<div>
@@ -18,13 +65,15 @@ class SignUp extends React.Component
                     </ul>
                 </div>
                 <div>
-                Username: <input id="username" type="text" name="username"></input>
-                Email address: <input id="email" type="email" name="email"></input>
-                Password: <input id="password" type="password" name="password"></input>
+                <form onSubmit={this.onSubmit}>
+                Username: <input id="username" type="text" value={this.state.name} onChange={this.onChangeUserName} name="username"/>
+                Email address: <input id="email" type="email" value={this.state.email} onChange={this.onChangeUserEmail} name="email"/>
+                Password: <input id="password" type="password" value={this.state.password} onChange={this.onChangeUserPassword} name="password"/>
                 <div id="input">
-                    <button id="loginbtn">Register</button>
+                    <input type="submit" id="loginbtn" value="Register"/>
                     <a href="#">Forgot Password </a>
                 </div>
+                </form>
                 </div>
             </span> 
         </div>
@@ -34,4 +83,4 @@ class SignUp extends React.Component
     }
 }
 
-export default SignUp;
+
