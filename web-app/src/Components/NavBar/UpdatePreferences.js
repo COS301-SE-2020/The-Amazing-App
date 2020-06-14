88e2adbc5d159d133cd62a3dc490d3da90d0e6eb
Tshegofatso <u18189424@tuks,co.za>
2020-06-14 05:17:25 +0200

    User preferences functionality

--- web-app/src/Components/NavBar/UpdatePreferences.js
+++ web-app/src/Components/NavBar/UpdatePreferences.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Header, Modal, Icon, Button } from "semantic-ui-react";
+import {Header, Modal,  Form, Button } from "semantic-ui-react";
 
 
 
@@ -21,28 +21,90 @@ class UpdatePreferences extends React.Component {
            <span style={{margin: 50}}>
                 Username: TheTshegofatso
                 <br></br>
-                <Button size='medium' style={{fontSize: 13, backgroundColor: "#2A9D8F",color: "white", marginLeft: 50, marginTop: 20, marginBottom: 20}}>
+                <Modal
+                  style={{marginTop: 100, top: "auto",bottom: "auto",left: "auto",right: "auto",position: "relative",height: 200, width: 400}} 
+                  trigger ={<Button size='medium' style={{fontSize: 13, backgroundColor: "#2A9D8F",color: "white", marginLeft: 50, marginTop: 20, marginBottom: 20}}>
                 <i className="users icon"></i>
                     Update Username
-                </Button>
+                </Button>} >
+                <Modal.Header>Update Username</Modal.Header>
+        <Modal.Content >
+          <Modal.Description>
+          <div>
+              <Form size="small" style={{width: 200}} >
+                <Form.Field
+                  label='New Username'
+                  control='input'
+                  placeholder='New username'  
+                />
+                 <button className="ui button" style={{backgroundColor: "#2A9D8F", color: "white"}}>Submit</button>
+              </Form>
+            </div>
+           </Modal.Description>
+          </Modal.Content>
+          </Modal>
             </span>
             <br></br>
             <span style={{margin: 50}}>
                 Username: Tshegofatso@gmail.com
                 <br></br>
-                <Button size='medium' style={{fontSize: 13, backgroundColor: "#2A9D8F",color: "white", marginLeft: 50, marginTop: 20, marginBottom: 20}}>
+                <Modal 
+                style={{marginTop: 100, top: "auto",bottom: "auto",left: "auto",right: "auto",position: "relative",height: 200, width:400}} 
+                trigger={<Button size='medium' style={{fontSize: 13, backgroundColor: "#2A9D8F",color: "white", marginLeft: 50, marginTop: 20, marginBottom: 20}}>
                 <i className="envelope icon"></i>
                     Update Email Address
-                </Button>
+                </Button>} >
+                <Modal.Header>Update Email Address</Modal.Header>
+        <Modal.Content >
+          <Modal.Description>
+          <div>
+              <Form size="small" style={{width: 200}} >
+                <Form.Field
+                  label='New Email Address'
+                  control='input'
+                  type='email'
+                  placeholder='New Email Address'  
+                />
+                 <button className="ui button" style={{backgroundColor: "#2A9D8F", color: "white"}}>Submit</button>
+              </Form>
+            </div>
+           </Modal.Description>
+          </Modal.Content>
+          </Modal>
             </span>
             <br></br>
             <span style={{margin: 50}}>
                 Password: ********
                 <br></br>
-                <Button size='medium' style={{fontSize: 13, backgroundColor: "#2A9D8F",color: "white", marginLeft:50, marginTop: 20, marginBottom: 20}}>
+                <Modal 
+                style={{marginTop: 100, top: "auto",bottom: "auto",left: "auto",right: "auto",position: "relative",height: 300, width:400}} 
+                trigger={<Button size='medium' style={{fontSize: 13, backgroundColor: "#2A9D8F",color: "white", marginLeft:50, marginTop: 20, marginBottom: 20}}>
                 <i className="lock icon"></i>
                     Update Password
-                </Button>
+                </Button>} >
+                <Modal.Header>Update Password</Modal.Header>
+                <Modal.Content >
+                <Modal.Description>
+                <div>
+                <Form size="small" style={{width: 200}} >
+                <Form.Field
+                  label='New Password'
+                  control='input'
+                  type= 'password'
+                  placeholder='New password'  
+                />
+                <Form.Field
+                  label='Confirm Password'
+                  control='input'
+                  type= 'password'
+                  placeholder='New Password'  
+                />
+                 <button className="ui button" style={{backgroundColor: "#2A9D8F", color: "white"}}>Submit</button>
+              </Form>
+            </div>
+           </Modal.Description>
+          </Modal.Content>
+          </Modal>
             </span>
           </Modal.Description>
         </Modal.Content>
