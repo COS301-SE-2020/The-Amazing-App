6ca920852877008c78a3f08808984e23bc77f23e
thamie323 <62136298+thamie323@users.noreply.github.com>
2020-07-23 07:16:26 -0700

    unit testing implementation

--- web-app/src/Components/Login/__test__/Forgot.test.js
+++ web-app/src/Components/Login/__test__/Forgot.test.js
@@ -2,21 +2,21 @@ const { Item } = require("semantic-ui-react")
 import React from 'react';
 import Enzyme,{shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import SignIn from '../SignIn';
+import Forgot from '../Forgot';
 import ReactDOM from 'react-dom';
 Enzyme.configure({ adapter: new Adapter() });
 
 
-describe('Login',()=>
+describe('Forgot',()=>
 {
      it("renders without crashing", ()=>{
-        const wrapper = shallow(<SignIn />);
+        const wrapper = shallow(<Forgot />);
         const page = true;
         const text = wrapper.find('div');
         expect(page).toBe(true) ;
     })
     it('should show buuton',()=>{
-        const wrapper = shallow(<SignIn />);
+        const wrapper = shallow(<Forgot />);
         
         const button = wrapper.find('.loginbtn').debug();
         console.log(button);
@@ -26,14 +26,14 @@ describe('Login',()=>
 
     })
    it('should show text',()=>{
-       const wrapper = shallow(<SignIn />); 
+       const wrapper = shallow(<Forgot />); 
        const text = wrapper.find('p');
        expect('Forgot Password').toBe('Forgot Password');
 
    })
 
    it('should show input box',()=>{
-    const wrapper = shallow(<SignIn />);
+    const wrapper = shallow(<Forgot />);
     const text = wrapper.find('input');
     expect(true).toBe(true);
 
