2dbbd6b27bedb67fa06665c01a64170fe0607b44
Tshegofatso <u18189424@tuks,co.za>
2020-08-20 00:13:13 +0200

    Removed yellowBox from Expo Client

--- app/App.js
+++ app/App.js
@@ -2,6 +2,8 @@ import { createAppContainer,createSwitchNavigator } from 'react-navigation';
 import main from './src/Router/MainRouter';
 import auth from './src/Router/AuthRouter';
 import landingScreen from './src/Screens/LandingScreen';
+
+console.disableYellowBox = true; 
 const navigator = createSwitchNavigator( {
     landingScreen,
     auth,
