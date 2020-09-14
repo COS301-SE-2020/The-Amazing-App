d03d6e45e9467a117460fa3149dfa0d73f499ce6
thamie323 <62136298+thamie323@users.noreply.github.com>
2020-09-14 02:43:30 -0700

    unit testing for the navbar component and the footer component.

--- web-app/src/Components/NavBar/__test__/navBar.test.js
+++ web-app/src/Components/NavBar/__test__/navBar.test.js
@@ -1,34 +1,46 @@
 const { Item } = require("semantic-ui-react")
 import React from 'react';
-import Enzyme,{shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
+import Enzyme,{mount ,shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavBar from '../navBar';
+import Footer from '../Footer'
+import SignIn from '../../Login/SignIn'
  
 Enzyme.configure({ adapter: new Adapter() });
 
 
-describe('Footer',()=>
+describe('Login component',()=>
 {
-    it('should be show text',()=>{
-        const wrapper = shallow(<NavBar />);
-        const text_ = wrapper.find('Nav.Link Nav.Link');
-        const text = true;
-        expect(text).toBe(true);
-    });
+    it('it should render',()=>{
+        const component = renderer.create(<NavBar />);
 
-    it('Testing the button',()=>{
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    
+
+    it('should render required buttons',()=>{
         const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
+        expect(button.find('Button').length).toBe(4);
+        //expect(button_.length).toBe(1);
     });
 
-
-    it('Testing logo link',()=>{
-        const log = shallow(<NavBar />);
-        const logo_ = log.find('Nav.Link Nav.Link');
-        const logo__ = true;
-        expect(logo__).toBe(true);
+    it('should render required Links', () => {
+        const calculator = mount(<NavBar />);
+        // expect(links.length).toBe(0);
+        const length = 0;
+       // expect(form.find('input').length).toBe(2);
+        expect(calculator.find('Nav.Link').length).toBe(length);
+       
+        //expect(form.find('button').length).toBe(1);
+       // expect(form.find('p.result').length).toBe(1);
+      });
+    it('Testing the Navbar brand',()=>{
+        const brand = shallow(<NavBar />);
+        expect(brand.find('Navbar.brand').length).toBe(0);
+        //const logo__ = true;
+      //  expect(logo__).toBe(true);
     });
 
     it('Testing footer text',()=>{
@@ -38,68 +50,8 @@ describe('Footer',()=>
         expect(footer__).toBe(true);
     });
 
-    it('Testing footer bacground color',()=>{
-        const color = shallow(<NavBar />);
-        const color_ = color.find('Nav.Link Nav.Link');
-        const color__ = true;
-        expect(color__).toBe(true);
-    });
-
-    it('Testing footer font size',()=>{
-        const font = shallow(<NavBar />);
-        const font_ = font.find('Nav.Link Nav.Link');
-        const font__ = true;
-        expect(font__).toBe(true);
-    });
-
-    it('should be able to show the logo',()=>{
-        const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
-    });
-
-    it('Testing if the links',()=>{
-        const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
-    });
-
-    it('should show text',()=>{
-        const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
-    });
-
-    it('should show text in buttons',()=>{
-        const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
-    });
-
-    it('Testing button links',()=>{
-        const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
-    });
-
-    it('should show text',()=>{
-        const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
-    });
-
-    it('Testing links to home pages',()=>{
-        const button = shallow(<NavBar />);
-        const button_ = button.find('Nav.Link Nav.Link');
-        const button__ = true;
-        expect(button__).toBe(true);
-    });
+   
+    
 
 
 })
