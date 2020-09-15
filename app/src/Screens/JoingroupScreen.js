c3151119798e2e2c956ae02d5fcf5f831662e7ef
Tshegofatso <u18189424@tuks,co.za>
2020-09-16 01:22:13 +0200

    Updated join group screen

--- app/src/Screens/JoingroupScreen.js
+++ app/src/Screens/JoingroupScreen.js
@@ -2,22 +2,48 @@ import React ,{  } from 'react';
 import {View, StyleSheet, TouchableOpacity, ImageBackground , Image} from 'react-native'
 import { Input,Text, Button, Header,Icon} from 'react-native-elements';
 import FooterComponent from '../Componets/FooterComponent';
-import {getPicture} from '../Api/UserAPI'
+import { FontAwesome,MaterialIcons, FontAwesome5,SimpleLineIcons } from '@expo/vector-icons'; 
 import { StatusBar } from 'expo-status-bar';
+
 
 const JoingroupScreen = ({navigation})=>{
     return(
        <>
            <StatusBar style='#2A9D8F'/>
             <Header
-                centerComponent={{ text: 'Create Group', style: { color: '#fff',fontSize:22, fontWeight:'bold' } }}
+                centerComponent={{ text: 'Join Group', style: { color: '#fff',fontSize:22, fontWeight:'bold' } }}
                 rightComponent={
                     <TouchableOpacity onPress={()=>navigation.navigate('Profile')}>
                     </TouchableOpacity>
                 }
                 containerStyle={{backgroundColor:'#2A9D8F'}}
             />
-            <FooterComponent />
+            <View style={style.overLayer}>
+           <View style={style.titleContainer}>
+                <Text style={style.titleStyle}>Group Details</Text>
+            </View>
+            <View style={style.blade} >
+
+            </View>
+            <View style={style.detailContainer}>
+            <Input containerStyle={style.inputStyle}
+                placeholderTextColor='white'
+                placeholder='Group ID'leftIcon={
+                 <SimpleLineIcons name="globe-alt" size={24} color="white" />
+                }
+                
+                />
+                <Button
+                    buttonStyle={style.buttonStyle}
+                    title="Submit"
+                    onPress={()=>{
+                        creater()
+                        navigation.navigate('Dashboard')
+                    }
+                    }
+                />
+            </View>
+            </View>
         </>
     )
 }
@@ -29,8 +55,58 @@ const style = StyleSheet.create({
         borderRadius:20,
         borderWidth:3,
         borderColor:'#fff',
-
     },
+    titleStyle:{
+        fontSize:18,
+        fontWeight:'bold',
+        color:'white',
+    },
+    titleContainer:{
+        backgroundColor:'rgba(42, 157, 143, 0.7)',
+        alignItems:'center',
+        marginTop:30,
+        paddingBottom:15,
+        paddingTop:15,
+        marginLeft:35,
+        marginRight:35,
+        borderRadius:5
+    },
+    detailContainer:{
+        backgroundColor:'rgba(42, 157, 143, 0.7)',
+        alignItems:'center',
+        paddingBottom:15,
+        paddingTop:15,
+        marginLeft:35,
+        marginRight:35,
+        borderRadius:5
+    },
+    inputStyle:{
+        width:'70%',
+        alignSelf:'center',
+    },
+    buttonStyle:{
+        width:150,
+        height:45,
+        marginBottom:35,
+        alignSelf:'center',
+        borderRadius:10,
+        backgroundColor: "#2A9D8F",
+        marginTop:15
+    },
+    overLayer:{
+        width:'100%',
+        height:'100%',
+        backgroundColor:'rgba(42, 157, 143, 0.01)',
+    },
+    blade:{
+        backgroundColor:'white',
+        alignItems:'center',
+        paddingBottom:15,
+        paddingTop:15,
+        marginLeft:35,
+        marginRight:35,
+        borderRadius:5,
+    }
 })
 
 
