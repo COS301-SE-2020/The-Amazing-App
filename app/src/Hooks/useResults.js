dc86ce0148adbe2c064337518d0bfe4275972f17
Tshegofatso <u18189424@tuks,co.za>
2020-09-16 17:01:01 +0200

    Update dashboard to show user's groups

--- app/src/Hooks/useResults.js
+++ app/src/Hooks/useResults.js
@@ -1,14 +1,18 @@
-import  React, {useState} from 'react';
+import  React, {useState,useContext} from 'react';
 import firebase from '../Config/Config';
+import {AuthContext} from '../Context/AuthContext';
 
 export default ()=>{
     const [results,setResults] = useState([]);
     const [ games, setGames] = useState([]);
+    const authContext = useContext(AuthContext);
 
     const getGroups =async()=>{
         try{
-            const querySnapshot = await firebase.firestore().collection('groups').get();
-            setResults(querySnapshot.docs);
+            const query1 = await firebase.firestore().collection('groups').where("userId", '==', authContext.userId ).get();
+            const query2 = await firebase.firestore().collection('groups').where("member", "array-contains", authContext.userId ).get();
+            const groups = query1.docs.concat(query2.docs);
+            setResults(groups);
         }
         catch(eror){
             console.log('Something went wrong')
